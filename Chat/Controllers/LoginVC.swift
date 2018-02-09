32255d5af4044994f6a8ded60073af2bce19953d
Sateesh Peddaiahgari <Sateesh@Lakshmans-MacBook-Pro.local>
2018-02-09 01:16:23 -0600

    Added Login Controller

---
+++ Chat/Controllers/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Chat
+//
+//  Created by Sateesh Peddaiahgari on 2/9/18.
+//  Copyright © 2018 Sateesh Peddaiahgari. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
